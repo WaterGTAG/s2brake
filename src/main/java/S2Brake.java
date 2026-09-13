7328a4c2cac2a5527d76f79c51e36a93513f5acf
WaterGTAG <wassup@watergtag.com>
2026-09-13 10:40:11 -0700

    1.2 with dust particle color set

--- src/main/java/S2Brake.java
+++ src/main/java/S2Brake.java
@@ -5,6 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInputEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Particle;
+import org.bukkit.Color;
 
 public class S2Brake extends JavaPlugin implements Listener {
     private double speed;
@@ -17,13 +18,14 @@ public class S2Brake extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerInput (PlayerInputEvent event) {
         Player player = event.getPlayer();
+        Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(255, 48, 28), 2.0f);
         // Check if server has brakes on, then if player is in a minecart
         if (getConfig().getBoolean("brake.enabled")) {
         if (player.getVehicle() instanceof Minecart minecart) {
             while (event.getInput().isBackward()) {
                 minecart.setVelocity(minecart.getVelocity().multiply(speed));
                 if (getConfig().getBoolean("particles.enabled")) {
-                    player.spawnParticle(Particle.DUST, player.getLocation(), 1, 0.5, 1, 0.5, 0.01);
+                    player.spawnParticle(Particle.DUST, player.getLocation(), 1, 0.5, 1, 0.5, 0.01, dustOptions);
                 }
             }
         }
